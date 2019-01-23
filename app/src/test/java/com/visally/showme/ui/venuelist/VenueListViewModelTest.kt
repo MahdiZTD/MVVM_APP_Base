1535a85035b25a3e8b599d26e011531f550a3b2e
Mahdi Zare tahghigh doost <tahghighdoost@hamisystem.ir>
2019-01-23 17:07:18 +0330

    adding better test

--- app/src/test/java/com/visally/showme/ui/venuelist/VenueListViewModelTest.kt
+++ app/src/test/java/com/visally/showme/ui/venuelist/VenueListViewModelTest.kt
@@ -1,9 +1,12 @@
 package com.visally.showme.ui.venuelist
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.Observer
 import com.visally.showme.infrustructure.data.DataManager
 import com.visally.showme.infrustructure.data.model.api.searchvenue.SearchVenueResponse
 import com.visally.showme.infrustructure.data.model.api.venuedetail.VenueDetailResponse
+import com.visally.showme.infrustructure.data.model.db.VenueModel
 import com.visally.showme.infrustructure.utils.AppConstants
 import com.visally.showme.infrustructure.utils.getDate
 import com.visally.showme.infrustructure.utils.rx.TestSchedulerProvider
@@ -11,6 +14,7 @@ import io.reactivex.Maybe
 import io.reactivex.Single
 import io.reactivex.schedulers.TestScheduler
 import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertNotNull
 import org.junit.*
 
 import org.junit.runner.RunWith
@@ -18,6 +22,7 @@ import org.mockito.ArgumentMatchers
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
 import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
 
@@ -61,7 +66,7 @@ class VenueListViewModelTest {
     }
 
     @Test
-    fun getNearVenueTest_positiveResponse() {
+    fun getNearVenueTest_successResponse() {
         val lat = "35.797834"
         val lng = "51.43404"
         val page = 1
@@ -77,10 +82,14 @@ class VenueListViewModelTest {
                         AppConstants.CLIET_SECRET, date, AppConstants.PAGE_LIMIT, page * AppConstants.PAGE_LIMIT))
                 .thenReturn(Single.just(SearchVenueResponse()))
 
+        val observer = mock(Observer::class.java) as Observer<MutableList<VenueModel>>
+        mVenueListViewModel.mVenueModels.observeForever(observer)
+
         val spiedViewModel = com.nhaarman.mockitokotlin2.spy(mVenueListViewModel)
         spiedViewModel.dataManager.getNearVenueByLocationFromApi(location, AppConstants.CLIET_ID,
                 AppConstants.CLIET_SECRET, date, AppConstants.PAGE_LIMIT, page * AppConstants.PAGE_LIMIT)
 
+        assertNotNull(mVenueListViewModel.mVenueModels)
         mTestScheduler.triggerActions()
 
     }
